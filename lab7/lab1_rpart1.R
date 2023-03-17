36a1c76c9d03b461d66057deabb0a76dcaeb84d2
keerti-sundaram <70778626+keerti-sundaram@users.noreply.github.com>
2023-03-17 15:43:40 -0400

    Update lab1_rpart1.R

--- lab7/lab1_rpart1.R
+++ lab7/lab1_rpart1.R
@@ -3,3 +3,11 @@ swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swis
 plot(swiss_rpart) # try some different plot options
 text(swiss_rpart) # try some different text options
 
+#trying with abalone data
+abalone <- read.csv(url("https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data"), header = FALSE, sep = ",")
+head(abalone)
+#assign column names using concatenation function
+colnames(abalone) <- c("sex", "length", 'diameter', 'height', 'whole_weight', 'shucked_wieght', 'viscera_wieght', 'shell_weight', 'rings' )
+abalone_rpart <- rpart(rings ~ length + diameter, data = abalone)
+plot(abalone_rpart)
+text(abalone_rpart)
