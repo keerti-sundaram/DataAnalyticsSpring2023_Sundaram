ed1777f61117d27f8b37530936d59f2072f07429
keerti-sundaram <70778626+keerti-sundaram@users.noreply.github.com>
2023-03-18 15:06:12 -0400

    Add files via upload

---
+++ lab7/titanic.R
@@ -0,0 +1,25 @@
+library(titanic)
+
+require(rpart)
+str(Titanic)
+titanic_rpart <- rpart(Survived ~., data = Titanic)
+
+plot(titanic_rpart) 
+text(titanic_rpart)
+rpart.plot(titanic_rpart)
+
+require(party)
+treeTitanic <- ctree(Survived ~., data=Titanic)
+plot(treeTitanic)
+
+cforest(Survived ~., data=Titanic, controls=cforest_control(mtry=2, mincriterion=0))
+
+help(hclust)
+dist_mat <- dist(Titanic, method = 'euclidean')
+dist_mat
+
+set.seed(240)
+h_cl <- hclust(dist_mat, method="average")
+h_cl
+
+plot(h_cl)
