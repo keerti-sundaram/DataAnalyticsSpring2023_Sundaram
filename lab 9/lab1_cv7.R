df901d5b675b6e4ea28e647816f5b70e40707541
keerti-sundaram <70778626+keerti-sundaram@users.noreply.github.com>
2023-04-02 10:12:16 -0400

    Delete lab 9 directory

--- lab 9/lab1_cv7.R
+++
@@ -1,5 +0,0 @@
-set.seed(2143) # set seed for reproducibility
-cvFolds(20, K = 5, type = "random")
-cvFolds(20, K = 5, type = "consecutive")
-cvFolds(20, K = 5, type = "interleaved")
-cvFolds(20, K = 5, R = 10)
