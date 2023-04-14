8753eb8a21a4330011b67a2bc916678215c0a295
keerti-sundaram <70778626+keerti-sundaram@users.noreply.github.com>
2023-04-14 15:44:55 -0400

    Add files via upload

--- final_project/Models and Analysis/kmeans1_project.R
+++ final_project/Models and Analysis/kmeans1_project.R
@@ -1,30 +1,16 @@
 copd <- read.csv("/Users/keertisundaram/Dropbox/Data Analytics/mortality_csv.csv", header=T)
-View(copd)
-str(copd)
-library(caret)
-library(RANN)
-#data imputation
-imp <- preProcess(copd, method = "medianImpute")
-copd_imp <- predict(imp, copd)
-View(copd_imp)
-#need to update grand totals
-no_labels <- copd_imp[-1]
-grand_total <- rowSums(no_labels[-length(no_labels)])
+#data imputation, nearest-neighbor imputation
+library(VIM)
+copd_imp <- kNN(copd[-length(copd)], k=5)
+summary(copd_imp)
+copd_imp <- subset(copd_imp, select=Country:X2020)
+grand_total <- rowSums(copd_imp[-1])
 copd_imp$Grand.Total <- grand_total
 View(copd_imp)
 
 library(ISLR)
 set.seed(101)
-
-
-totalClusters <- kmeans(copd_imp$Grand.Total, 4, nstart = 20) # nstart is the number of random start
-print(totalClusters)
-table(totalClusters$cluster)
 library(cluster)
-clusplot(copd_imp,totalClusters$cluster, color = TRUE, shade = TRUE, labels = 0, lines = 0)
-
-
-
 #find optimal number of clusters
 library(factoextra)
 fviz_nbclust(copd_imp[-1], kmeans, method="wss") #bend at 4
@@ -33,7 +19,7 @@ print(totalClusters$cluster)
 #new data frame matching country to cluster 
 df <- data.frame(copd_imp$Country, totalClusters$cluster)
 df
-clusplot(copd_imp,totalClusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(copd_imp,totalClusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0,  main = "Number of Deaths Due to COPD from 2000-2020")
 
 group1<- data.frame(copd_imp$Country, copd_imp[,c(2:6)])
 group2<- data.frame(copd_imp$Country, copd_imp[,c(7:11)])
@@ -41,18 +27,104 @@ group3<- data.frame(copd_imp$Country, copd_imp[,c(12:16)])
 group4<- data.frame(copd_imp$Country, copd_imp[,c(17:22)])
 
 group1Clusters <- kmeans(group1[-1], 4, nstart = 20) 
-clusplot(group1,group1Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(group1,group1Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0, main = "Number of Deaths Due to COPD from 2000-2004")
 group1_clusters <- data.frame(copd_imp$Country, group1Clusters$cluster)
 group1_clusters
 
 group2Clusters <- kmeans(group2[-1], 4, nstart = 20) 
-clusplot(group2,group2Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(group2,group2Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0, main = "Number of Deaths Due to COPD from 2005-2009")
 group2_clusters <- data.frame(copd_imp$Country, group2Clusters$cluster)
 
 group3Clusters <- kmeans(group3[-1], 4, nstart = 20) 
-clusplot(group3,group3Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(group3,group3Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0, main = "Number of Deaths Due to COPD from 2010-2014")
 group3_clusters <- data.frame(copd_imp$Country, group3Clusters$cluster)
 
 group4Clusters <- kmeans(group4[-1], 4, nstart = 20) 
-clusplot(group4,group4Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(group4,group4Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0, main = "Number of Deaths Due to COPD from 2015-2020")
 group4_clusters <- data.frame(copd_imp$Country, group4Clusters$cluster)
+
+colnames(df) <- c("Country", "Cluster")
+colnames(group1_clusters) <- c("Country", "Cluster")
+colnames(group2_clusters) <- c("Country", "Cluster")
+colnames(group3_clusters) <- c("Country", "Cluster")
+colnames(group4_clusters) <- c("Country", "Cluster")
+
+Countries_GT_1 <- (df[df$Cluster == 1,])$Country
+Countries_GT_2 <- (df[df$Cluster == 2,])$Country
+Countries_GT_3  <- (df[df$Cluster == 3,])$Country
+Countries_GT_4 <-  (df[df$Cluster == 4,])$Country
+
+#group 1
+Countries_G1_1 <-  (group1_clusters[group1_clusters$Cluster == 1,])$Country
+Countries_G1_2 <-  (group1_clusters[group1_clusters$Cluster == 2,])$Country
+Countries_G1_3 <-  (group1_clusters[group1_clusters$Cluster == 3,])$Country
+Countries_G1_4 <-  (group1_clusters[group1_clusters$Cluster == 4,])$Country
+
+#group 2
+Countries_G2_1 <-  (group2_clusters[group2_clusters$Cluster == 1,])$Country
+Countries_G2_2 <-  (group2_clusters[group2_clusters$Cluster == 2,])$Country
+Countries_G2_3 <-  (group2_clusters[group2_clusters$Cluster == 3,])$Country
+Countries_G2_4 <- (group2_clusters[group2_clusters$Cluster == 4,])$Country
+
+#group 3
+Countries_G3_1 <-  (group3_clusters[group3_clusters$Cluster == 1,])$Country
+Countries_G3_2 <-  (group3_clusters[group3_clusters$Cluster == 2,])$Country
+Countries_G3_3 <-  (group3_clusters[group3_clusters$Cluster == 3,])$Country
+Countries_G3_4 <-  (group3_clusters[group3_clusters$Cluster == 4,])$Country
+
+#group 4
+Countries_G4_1 <-  (group4_clusters[group4_clusters$Cluster == 1,])$Country
+Countries_G4_2 <-  (group4_clusters[group4_clusters$Cluster == 2,])$Country
+Countries_G4_3 <-  (group4_clusters[group4_clusters$Cluster == 3,])$Country
+Countries_G4_4 <-  (group4_clusters[group4_clusters$Cluster == 4,])$Country
+
+cat(Countries_GT_1, sep="\n")
+cat(Countries_GT_2, sep="\n")
+cat(Countries_GT_3, sep="\n")
+cat(Countries_GT_4, sep="\n")
+
+setdiff(Countries_GT_1, Countries_G1_3)
+setdiff(Countries_GT_2, Countries_G1_4)
+setdiff(Countries_GT_3, Countries_G1_1)
+setdiff(Countries_GT_4, Countries_G1_2)
+
+setdiff(Countries_G1_1, Countries_G2_2)
+setdiff(Countries_G1_2, Countries_G2_3)
+setdiff(Countries_G1_3, Countries_G2_4)
+setdiff(Countries_G1_4, Countries_G2_1)
+
+setdiff(Countries_G2_1, Countries_G3_2)
+setdiff(Countries_G2_2, Countries_G3_4)
+setdiff(Countries_G2_3, Countries_G3_1)
+setdiff(Countries_G2_4, Countries_G3_3)
+
+setdiff(Countries_G3_1, Countries_G4_2)
+setdiff(Countries_G3_2, Countries_G4_1)
+setdiff(Countries_G3_3, Countries_G4_3)
+setdiff(Countries_G3_4, Countries_G4_4)
+
+clus1 <- (intersect((intersect((intersect(Countries_G1_1, Countries_G2_2)), Countries_G3_4)), Countries_G4_4))
+
+clus2 <- (intersect((intersect((intersect(Countries_G1_2, Countries_G2_3)), Countries_G3_1)), Countries_G4_2))
+
+clus3 <- (intersect((intersect((intersect(Countries_G1_3, Countries_G2_4)), Countries_G3_3)), Countries_G4_3))
+
+clus4 <- (intersect((intersect((intersect(Countries_G1_4, Countries_G2_1)), Countries_G3_2)), Countries_G4_1))
+
+
+cat(clus1, sep="\n")
+cat(clus2, sep="\n")
+cat(clus3, sep="\n")
+cat(clus4, sep="\n")
+
+countries_clustered <- c(clus1, clus2, clus3, clus4)
+total_countries <- copd_imp$Country
+#14 countries "switched"
+cat((setdiff(total_countries, countries_clustered)), sep="\n")
+
+group1_clusters[group1_clusters$Country == "Turkey", ]
+group2_clusters[group2_clusters$Country == "Turkey", ]
+group3_clusters[group3_clusters$Country == "Turkey", ]
+group4_clusters[group4_clusters$Country == "Turkey", ]
+
+Countries_GT_1
--- final_project/Models and Analysis/kmeans2_project.R
+++ final_project/Models and Analysis/kmeans2_project.R
@@ -1,16 +1,17 @@
 gdp <- read.csv("/Users/keertisundaram/Dropbox/Data Analytics/gdp_csv.csv", header=T)
-View(gdp)
-library(caret)
-library(RANN)
-#data imputation
-imp <- preProcess(gdp, method = "medianImpute")
-gdp_imp <- predict(imp, gdp)
-View(gdp_imp)
-#need to update grand totals
-no_labels <- gdp_imp[-1]
-grand_total <- rowSums(no_labels[-length(no_labels)])
+library(VIM)
+gdp_imp <- kNN(gdp[-length(gdp)], k=5)
+gdp_imp <- subset(gdp_imp, select=Country:X2020)
+grand_total <- rowSums(gdp_imp[-1])
 gdp_imp$Grand.Total <- grand_total
-View(gdp_imp)
+avg_values <- data.frame(gdp_imp$Country, rowMeans(gdp_imp[-1]))
+colnames(avg_values) <- c("Country", "Average")
+#using sum of values
+hist(gdp_imp$Grand.Total, col='#25A476', main="Sum of Percent of GDP spent on Health Care over 2000-2020")
+boxplot(gdp_imp$Grand.Total, main="Sum of Percent of GDP spent on Health Care over 2000-2020" )
+#using avg of values
+hist(avg_values$Average, col='#25A476', main="Averages of Percent of GDP spent on Health Care over 2000-2020")
+boxplot(avg_values$Average, main="Averages of Percent of GDP spent on Health Care over 2000-2020" )
 
 library(ISLR)
 library(cluster)
@@ -27,7 +28,7 @@ clusplot(gdp_imp,totalClusters$cluster, color = TRUE, shade = TRUE, labels = 4, 
 fviz_nbclust(gdp_imp[-1], kmeans, method="wss") #bend at 4
 totalClusters <- kmeans(gdp_imp[-1], 4, nstart = 20) # nstart is the number of random start
 print(totalClusters$cluster)
-clusplot(gdp_imp,totalClusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(gdp_imp,totalClusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0, main = "Percent of GDP Spent on Health Care 2000-2020")
 df <- data.frame(gdp_imp$Country, totalClusters$cluster)
 df
 #split into groups
@@ -35,24 +36,24 @@ group1<- data.frame(gdp_imp$Country,gdp_imp[,c(2:6)])
 group2<- data.frame(gdp_imp$Country, gdp_imp[,c(7:11)])
 group3<- data.frame(gdp_imp$Country, gdp_imp[,c(12:16)])
 group4<- data.frame(gdp_imp$Country, gdp_imp[,c(17:22)])
-
+group4
 fviz_nbclust(group4[-1], kmeans, method="wss") #bend at 4
 
 group1Clusters <- kmeans(group1[-1], 4, nstart = 20) 
-clusplot(group1,group1Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(group1,group1Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0, main = "Percent of GDP Spent on Health Care 2000-2004")
 group1_clusters <- data.frame(gdp_imp$Country, group1Clusters$cluster)
 group1_clusters
 
 group2Clusters <- kmeans(group2[-1], 4, nstart = 20) 
-clusplot(group2,group2Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(group2,group2Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0,main = "Percent of GDP Spent on Health Care 2005-2009")
 group2_clusters <- data.frame(gdp_imp$Country, group2Clusters$cluster)
 
 group3Clusters <- kmeans(group3[-1], 4, nstart = 20) 
-clusplot(group3,group3Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(group3,group3Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0, main = "Percent of GDP Spent on Health Care 2010-2014")
 group3_clusters <- data.frame(gdp_imp$Country, group3Clusters$cluster)
 
 group4Clusters <- kmeans(group4[-1], 4, nstart = 20) 
-clusplot(group4,group4Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0)
+clusplot(group4,group4Clusters$cluster, color = TRUE, shade = TRUE, labels = 4, lines = 0, main = "Percent of GDP Spent on Health Care 2015-2020")
 group4_clusters <- data.frame(gdp_imp$Country, group4Clusters$cluster)
 
 #renaming columns for simplicity
@@ -64,6 +65,7 @@ colnames(group4_clusters) <- c("Country", "Cluster")
 
 #Grand total
 Countries_GT_1 <- (df[df$Cluster == 1,])$Country
+Countries_GT_1
 Countries_GT_2 <- (df[df$Cluster == 2,])$Country
 Countries_GT_3  <- (df[df$Cluster == 3,])$Country
 Countries_GT_4 <-  (df[df$Cluster == 4,])$Country
@@ -92,7 +94,60 @@ Countries_G4_2 <-  (group4_clusters[group4_clusters$Cluster == 2,])$Country
 Countries_G4_3 <-  (group4_clusters[group4_clusters$Cluster == 3,])$Country
 Countries_G4_4 <-  (group4_clusters[group4_clusters$Cluster == 4,])$Country
 
-group1_clusters
-group2_clusters
-group3_clusters
-group4_clusters
+cat(Countries_GT_1, sep="\n")
+cat(Countries_GT_2, sep="\n")
+cat(Countries_GT_3, sep="\n")
+cat(Countries_GT_4, sep="\n")
+
+setdiff(Countries_GT_1, Countries_G1_1)
+setdiff(Countries_GT_2, Countries_G1_2)
+setdiff(Countries_GT_3, Countries_G1_3)
+setdiff(Countries_GT_4, Countries_G1_4)
+
+setdiff(Countries_G1_1, Countries_G2_4)
+setdiff(Countries_G1_2, Countries_G2_2)
+setdiff(Countries_G1_3, Countries_G2_1)
+setdiff(Countries_G1_4, Countries_G2_3)
+
+setdiff(Countries_G2_1, Countries_G3_2)
+setdiff(Countries_G2_2, Countries_G3_1)
+setdiff(Countries_G2_3, Countries_G3_4)
+setdiff(Countries_G2_4, Countries_G3_3)
+
+setdiff(Countries_G3_1, Countries_G4_2)
+setdiff(Countries_G3_2, Countries_G4_3)
+setdiff(Countries_G3_3, Countries_G4_1)
+setdiff(Countries_G3_4, Countries_G4_4)
+
+setdiff(Countries_GT_1, Countries_G4_1)
+setdiff(Countries_GT_2, Countries_G4_2)
+setdiff(Countries_GT_3, Countries_G4_3)
+setdiff(Countries_GT_4, Countries_G4_4)
+
+clus1 <- (intersect((intersect((intersect(Countries_G1_1, Countries_G2_4)), Countries_G3_3)), Countries_G4_1))
+clus1
+clus2 <- (intersect((intersect((intersect(Countries_G1_2, Countries_G2_2)), Countries_G3_1)), Countries_G4_2))
+clus2
+clus3 <- (intersect((intersect((intersect(Countries_G1_3, Countries_G2_1)), Countries_G3_2)), Countries_G4_3))
+clus3
+clus4 <- (intersect((intersect((intersect(Countries_G1_4, Countries_G2_3)), Countries_G3_4)), Countries_G4_4))
+clus4
+
+cat(clus1, sep="\n")
+cat(clus2, sep="\n")
+cat(clus3, sep="\n")
+cat(clus4, sep="\n")
+
+clus1
+nrow(df[df$Cluster == 1,])
+countries_clustered <- c(clus1, clus2, clus3, clus4)
+total_countries <- gdp_imp$Country
+#42 countries "switched"
+cat((setdiff(total_countries, countries_clustered)), sep="\n")
+str(total_countries)
+str(countries_clustered)
+
+group1_clusters[group1_clusters$Country == "Uruguay", ]
+group2_clusters[group2_clusters$Country == "Uruguay", ]
+group3_clusters[group3_clusters$Country == "Uruguay", ]
+group4_clusters[group4_clusters$Country == "Uruguay", ]
