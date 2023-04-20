5d280d5dc9f36845e543ab7950ed8b6e005d7222
keerti-sundaram <70778626+keerti-sundaram@users.noreply.github.com>
2023-04-20 17:25:05 -0400

    Update EDA_project.R

--- final_project/EDA/EDA_project.R
+++ final_project/EDA/EDA_project.R
@@ -1,3 +1,5 @@
+#investigating the data and looking at the structure
+#read in data
 copd <- read.csv("/Users/keertisundaram/Dropbox/Data Analytics/mortality_csv.csv", header=T)
 gdp <- read.csv("/Users/keertisundaram/Dropbox/Data Analytics/gdp_csv.csv", header=T)
 View(copd)
@@ -5,7 +7,7 @@ View(gdp)
 str(copd)
 library(caret)
 library(RANN)
-#data imputation
+#data imputation (technique using caret library, not used later)
 imp <- preProcess(copd, method = "medianImpute")
 copd_imp <- predict(imp, copd)
 View(copd_imp)
@@ -15,15 +17,17 @@ grand_total <- rowSums(no_labels[-length(no_labels)])
 copd_imp$Grand.Total <- grand_total
 View(demo_imp)
 
-
+#filtering data by country
 copd_Albania <- copd[copd$Country == "Albania",]
 names(copd_Albania) <- NULL
 
 #removing country label and grand total
 c_Albania <- copd_Albania[-1]
 c_Albania <- c_Albania[-length(c_Albania)]
+#histogram of individual country
 hist(as.numeric(c_Albania))
 c_Albania
+#gdp one country filtering
 gdp_Albania <- gdp[gdp$Country == "Albania",]
 gdp_Albania
 g_Albania <- gdp_Albania[!is.na(gdp_Albania)]
@@ -32,95 +36,27 @@ g_Albania
 g_Albania <- g_Albania[-1]
 g_Albania <- g_Albania[-length(g_Albania)]
 hist(as.numeric(g_Albania))
-years <- c("X2000","X2001","X2002","X2003","X2004","X2005","X2006","X2007","X2008","X2009","X2010","X2011","X2012","X2013","X2014","X2015","X2016","X2017","X2018","X2019","X2020")
 
-c_transpose = t(c_Albania)
-df <- data.frame(years, g_Albania, c_transpose)
-df
-
-
+#looking at a specific year
 tf <- is.na(copd$X2001)
 c_2001 <- copd$X2001[!tf]
 mean(c_2001)
 median(c_2001)
 hist(c_2001)
-
-tf <- is.na(copd$X2019)
-c_2019 <- copd$X2019[!tf]
-mean(c_2019)
-median(c_2019)
-hist(c_2019)
-boxplot(c_2001, c_2019)
-
-
-tf <- is.na(copd$X2005)
-c_2001 <- copd$X2005[!tf]
-mean(c_2001)
-median(c_2001)
-hist(c_2001)
-
-tf <- is.na(copd$X2015)
-c_2019 <- copd$X2015[!tf]
-mean(c_2019)
-median(c_2019)
-hist(c_2019)
-boxplot(c_2001, c_2019)
-
-
-tf <- is.na(copd$X2009)
-c_2001 <- copd$X2009[!tf]
-mean(c_2001)
-median(c_2001)
-hist(c_2001)
-
-tf <- is.na(copd$X2014)
-c_2019 <- copd$X2014[!tf]
-mean(c_2019)
-median(c_2019)
-hist(c_2019)
-boxplot(c_2001, c_2019)
-
-
+#looking at US data
 copd_us <- copd[copd$Country == "United States of America",]
 c_us <- copd_us[!is.na(copd_us)]
 c_us <- c_us[-1]
 c_us <- c_us[-length(c_us)]
 plot(as.numeric(c_us))
-
+#looking at GDP for a specific year
 tf <- is.na(gdp$X2001)
 g_2001<- gdp$X2001[!tf]
 mean(g_2001)
 median(g_2001)
 
-tf <- is.na(gdp$X2020)
-g_2020<- gdp$X2020[!tf]
-mean(g_2020)
-median(g_2020)
-boxplot(g_2001, g_2020)
-
-tf <- is.na(gdp$X2005)
-g_2001<- gdp$X2005[!tf]
-mean(g_2001)
-median(g_2001)
-
-tf <- is.na(gdp$X2015)
-g_2020<- gdp$X2015[!tf]
-mean(g_2020)
-median(g_2020)
-boxplot(g_2001, g_2020)
-
-tf <- is.na(gdp$X2008)
-g_2001<- gdp$X2008[!tf]
-mean(g_2001)
-median(g_2001)
-
-tf <- is.na(gdp$X2017)
-g_2020<- gdp$X2017[!tf]
-mean(g_2020)
-median(g_2020)
-boxplot(g_2001, g_2020)
-
-
+#used for assignment 5 presentation, looking at COPD deaths and percent spent on healthcare
+#for countries with top 5 highest PM2.5 mean exposure and lowest 5 PM2.5 mean exposure
 copd_China <- copd[copd$Country == "China, Hong Kong SAR",]
 copd_China
 
@@ -178,38 +114,30 @@ g_Bahamas <- gdp_Bahamas[!tf]
 g_Bahamas <- g_Bahamas[-length(g_Bahamas)]
 mean(as.numeric(g_Bahamas[-1]))
 
-gdp_Bahamas<- gdp[gdp$Country == "Finland",]
-gdp_Bahamas
-tf <- is.na(gdp_Bahamas)
-g_Bahamas <- gdp_Bahamas[!tf]
-g_Bahamas <- g_Bahamas[-length(g_Bahamas)]
-mean(as.numeric(g_Bahamas[-1]))
+gdp_Finland<- gdp[gdp$Country == "Finland",]
+gdp_Finland
+tf <- is.na(gdp_Finland)
+g_Finland <- gdp_Finland[!tf]
+g_Finland <- g_Finland[-length(g_Finlands)]
+mean(as.numeric(g_Finland[-1]))
 
-gdp_Bahamas<- gdp[gdp$Country == "Iceland",]
-gdp_Bahamas
-tf <- is.na(gdp_Bahamas)
-g_Bahamas <- gdp_Bahamas[!tf]
-g_Bahamas <- g_Bahamas[-length(g_Bahamas)]
-mean(as.numeric(g_Bahamas[-1]))
+gdp_Iceland<- gdp[gdp$Country == "Iceland",]
+gdp_Iceland
+tf <- is.na(gdp_Iceland)
+g_Iceland <- gdp_Iceland[!tf]
+g_Iceland <- g_Iceland[-length(g_Iceland)]
+mean(as.numeric(g_Iceland[-1]))
 
-gdp_Bahamas<- gdp[gdp$Country == "Sweden",]
-gdp_Bahamas
-tf <- is.na(gdp_Bahamas)
-g_Bahamas <- gdp_Bahamas[!tf]
-g_Bahamas <- g_Bahamas[-length(g_Bahamas)]
-mean(as.numeric(g_Bahamas[-1]))
+gdp_Sweden<- gdp[gdp$Country == "Sweden",]
+gdp_Sweden
+tf <- is.na(gdp_Sweden)
+g_Sweden <- gdp_Sweden[!tf]
+g_Sweden <- g_Sweden[-length(g_Sweden)]
+mean(as.numeric(g_Sweden[-1]))
 
-gdp_Bahamas<- gdp[gdp$Country == "Norway",]
-gdp_Bahamas
-tf <- is.na(gdp_Bahamas)
-g_Bahamas <- gdp_Bahamas[!tf]
-g_Bahamas <- g_Bahamas[-length(g_Bahamas)]
-mean(as.numeric(g_Bahamas[-1]))
-
-tf <- is.na(gdp$X2010)
-g_2010 <- gdp$X2010[!tf]
-sclass <- kmeans(g_2010, 3) 
-help(kmeans)
-table(sclass$cluster, g_2010)    
-plot(g_2010)
-text(g_2010, labels = gdp$Country)
+gdp_Norway<- gdp[gdp$Country == "Norway",]
+gdp_Norway
+tf <- is.na(gdp_Norway)
+g_Norway <- gdp_Norway[!tf]
+g_Norway <- g_Norway[-length(g_Norway)]
+mean(as.numeric(g_Norway[-1]))
